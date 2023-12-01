3404a9a97283111da72e7678f2465a10be9f24d5
zoe-lloyds <150355048+zoe-lloyds@users.noreply.github.com>
2023-12-01 14:51:09 +0000

    update

--- Chatbot.py
+++ Chatbot.py
@@ -1,80 +1,17 @@
-from openai import OpenAI
-import streamlit as st
+# Streamlit UI
+st.title("Question Answering with PaLM")
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# User input for context
+context = st.text_area("Context:", "The quick brown fox jumps over the lazy dog.")
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+# User input for message
+message = st.text_input("Your question:", "What is the meaning of life?")
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+# Button to generate response
+if st.button("Generate Response"):
+    # Generate response using PaLMWrapper
+    response = palm_wrapper.generate_response(context, message)
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
-
-from vertexai.preview.language_models import ChatModel
-
-class CustomPaLMWrapper:
-    def _init_(self):
-        self.chat_model = ChatModel.from_pretrained("chat-bison@001")
-        self.parameters = {
-            "temperature": 0.2,
-            "max_output_tokens": 256,
-            "top_p": 0.95,
-        }
-
-    def generate_response(self, context, message):
-        chat = self.chat_model.start_chat(context)
-        response = chat.send_message(message, **self.parameters)
-        return response.text
-
-
-from operator import itemgetter
-
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
-from langchain.vectorstores import FAISS
-
-vectorstore = FAISS.from_texts(
-    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
-)
-retriever = vectorstore.as_retriever()
-
-template = """Answer the question based only on the following context:
-{context}
-
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
-
-model = ChatOpenAI()
-
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
-
-chain.invoke("where did harrison work?")
-
-    'Harrison worked at Kensho.'
+    # Display the response
+    st.subheader("Response:")
+    st.write(response)
